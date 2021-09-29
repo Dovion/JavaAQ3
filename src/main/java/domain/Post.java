c87125f3b94c1a6b04a23d7a09de3ca8c79a8aca
Dovion <vyacheslav-98@yandex.ru>
2021-09-29 04:22:44 +0300

    Project created

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+    private int  postId;
+    private String date;
+    private String headingGroup;
+    private String content;
+    private String mediaFileUrl;
+    private String comment;
+    private int like;
+    private int repost;
+    private int viewed;
+
+
+}
---
+++ src/main/java/domain/Vk.java
@@ -0,0 +1,10 @@
+package domain;
+import domain.Post;
+public class Vk {
+     private Post[] posts;
+
+     public Post[] generatePost(){
+         //logic
+         return null;
+     }
+}
